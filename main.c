aa228ac286ee2b05641912e73f255eb4b5b91853
Robert Korhonen <rkorhone@c2r2p4.hive.fi>
2024-06-07 18:18:16 +0300

    test 6

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include "libft/libft.h"
 #include "minishell.h"
 
-// test comment 41233112313
+// this is not a test comment
 // test comment
 // My changes no thery are mine
 // execute all commands here
