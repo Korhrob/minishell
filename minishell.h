2832b1723cc3131628f850bdf03d36052892673c
Artur <vegis.artur@gmail.com>
2024-06-08 19:08:17 +0300

    added struct

--- minishell.h
+++ minishell.h
@@ -5,14 +5,25 @@
 # define BULTIN_ENV "env"
 # define BULTIN_HELP "help"
 # define BULTIN_EXIT "exit"
+# define BULTIN_PWD "pwd"
+# define BULTIN_UNSET "unset"
+# define BULTIN_EXPORT "export"
 
 typedef enum e_builtin_cmd
 {
 	CD,
 	ENV,
 	HELP,
-	EXIT
+	EXIT,
+	PWD,
+	UNSET,
+	EXPORT
 }	t_builtin_cmd;
+
+typedef struct s_runtime
+{
+	char	**env;
+}	t_runtime;
 
 // history
 void	record_history(char *line);
