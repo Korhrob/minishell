99ce5a3ba1f79b84b41a8cdff8570b7bfe52ae3c
Artur <vegis.artur@gmail.com>
2024-07-06 09:23:17 +0300

    Expansions fixed, still has debugging prints

--- builtins/builtin_echo.c
+++ builtins/builtin_echo.c
@@ -1,28 +1,31 @@
 #include "builtins.h"
 
-static void	quotation_trim(char **args)
-{
-	int i;
+// static void	quotation_trim(char **args)
+// {
+// 	int i;
 
-	i = 0;
-	while (args[i] != NULL)
-	{
-		if (args[i][0] == '\"')
-			args[i] = minitrim(args[i], '\"');
-		else
-			args[i] = minitrim(args[i], '\'');
-		i++;
-	}
-}
+// 	i = 0;
+// 	while (args[i] != NULL)
+// 	{
+// 		if (args[i][0] == '\"')
+// 			args[i] = minitrim(args[i], '\"');
+// 		else
+// 			args[i] = minitrim(args[i], '\'');
+// 		i++;
+// 	}
+// }
 
+// prints on standard output all strings from an array except
+// the first and any -n flags that remove newline
 void    cmd_echo(char **args)
 {
 	int i;
 
 	i = 1;
-	quotation_trim(args);
-	while (ft_strcmp(args[i], "-n") == 0)
-		i++;
+	// quotation_trim(args);
+	if (args[1])
+		while (ft_strcmp(args[i], "-n") == 0)
+			i++;
 	while (args[i] != NULL)
 	{
 		ft_printf("%s", args[i]);
@@ -37,5 +40,5 @@ void    cmd_echo(char **args)
 	}
 	else
 		ft_printf("\n");
-
+	printf("Echo SUCCESS\n");
 }
--- expansion.c
+++ expansion.c
@@ -1,6 +1,52 @@
 #include "minishell.h"
 #include <stdlib.h>
+#include <stdio.h>
 
+static int	calculate_len(char **array, int count)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (count > 0)
+	{
+		len = len + ft_strlen(array[i]);
+		i++;
+		count--;
+	}
+	return (len);
+}
+
+static char	*array_join_c(char **array, int count)
+{
+	char	*out;
+	int		i;
+	int		j;
+	int		n;
+
+	i = 0;
+	j = 0;
+	n = 0;
+	out = (char *)malloc(calculate_len(array, count) + 1);
+	if (!out)
+		return (NULL);
+	while (count > 0)
+	{
+		while (array[i][j] != 0)
+			out[n++] = array[i][j++];
+		i++;
+		j = 0;
+		count--;
+		printf("Joined string = [%s]\n", array[i]);
+		out[n] = 0;
+		printf("string joined = [%s]\n", out);
+	}
+	out[n] = 0;
+	return (out);
+}
+
+// free every second string in the array starting with first
 static void	free_expands(char **array)
 {
 	int i;
@@ -16,6 +62,7 @@ static void	free_expands(char **array)
 	free(array);
 }
 
+// using the key finds the correct value if it exists
 static char	*find_expansion(char *key, t_env **environ)
 {
 	int	i;
@@ -30,6 +77,7 @@ static char	*find_expansion(char *key, t_env **environ)
 	return (NULL);
 }
 
+// Creates the key for which were looking for in the environ
 static char	*expand(char *pipe, t_env **environ)
 {
 	char	*key;
@@ -38,14 +86,15 @@ static char	*expand(char *pipe, t_env **environ)
 	i = 0;
 	key = NULL;
 	while (ft_isalnum(pipe[i]) || pipe[i] == '-' || pipe[i] == '_')
-	{
-		key[i] = pipe[i];
 		i++;
-	}
-	key[i] = 0;
+	key = (char *)malloc(i+1);
+	ft_strlcpy(key, pipe, i+1);
+	printf("Pipe key = [%s]\n", key);
 	return (find_expansion(key, environ));
 }
 
+// iterates the pipe and creates an array with expandable portions
+// replaced with corresponding environment values
 static char **create_strings(char **splitpipe, char *pipe, t_env **environ)
 {
 	int i;
@@ -57,16 +106,28 @@ static char **create_strings(char **splitpipe, char *pipe, t_env **environ)
 	temp = pipe;
 	while (*temp != 0)
 	{
+		printf("temp = [%s]\n", temp);
 		if ((*temp == '$') && (*temp+1 != '$' || *temp-1 != '$'))
 		{
+			printf("Pipe remainder = [%s] with len = %i\n", pipe, len);
 			splitpipe[i] = ft_strldup(pipe, 0, len);
+			printf("array string %i = [%s]\n", i, splitpipe[i]);
 			splitpipe[i+1] = expand(temp+1, environ);
+			printf("array string %i = [%s]\n", i+1, splitpipe[i+1]);
 			while (ft_isalnum(*temp+1) || *temp+1 == '-' || *temp+1 == '_')
 				temp++;
+			printf("temp = [%s]\n", temp);
 			i = i + 2;
-			pipe = temp;
+			pipe = temp+1;
 			len = -1;
+			while (ft_isalnum(*pipe) || *pipe == '-' || *pipe == '_')
+			{
+				pipe++,
+				len--;
+			}
+			printf("--------------------------\n");
 		}
+		printf("len = %i", len);
 		len++;
 		temp++;
 	}
@@ -74,6 +135,7 @@ static char **create_strings(char **splitpipe, char *pipe, t_env **environ)
 	return (splitpipe);
 }
 
+// base logic behind expanding the pipes
 static char	*expand_logic(char *pipe, t_env **environ)
 {
 	char **splitpipe;
@@ -88,19 +150,18 @@ static char	*expand_logic(char *pipe, t_env **environ)
 			count++;
 		i++;
 	}
+	printf("Quantity of expansions = %i\n", count);
 	if (count == 0)
 		return (pipe);
 	splitpipe = (char **)malloc(sizeof(char *)*(count * 2 + 1));
 	splitpipe = create_strings(splitpipe, pipe, environ);
 	free (pipe);
-	pipe = ft_array_join(splitpipe);
+	pipe = array_join_c(splitpipe, count * 2);
 	free_expands(splitpipe);
-	// Free every second string in the array and the array
 	return (pipe);
 }
 
-#include <stdio.h>
-
+// iterates every single pipe string and expands $ signs with environments
 int expand_dollars(char **pipes, t_env **environ)
 {
 	int	i;
@@ -113,5 +174,6 @@ int expand_dollars(char **pipes, t_env **environ)
 		printf("string after = [%s]\n", pipes[i]);
 		i++;
 	}
+	printf("expansion SUCCESS\n");
 	return (0);
 }
